189fc7febe62fc02190423c58f5a4f07259cb55e
kindsenior <kunio.einstein@gmail.com>
2015-03-08 17:27:02 +0900

    add asm load text box

--- src/WindowsFormsApplication1/Form1.Designer.cs
+++ src/WindowsFormsApplication1/Form1.Designer.cs
@@ -28,20 +28,73 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.TextboxDestAsm = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.ButtonLoadDestAsm = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // TextboxDestAsm
+            // 
+            this.TextboxDestAsm.AllowDrop = true;
+            this.TextboxDestAsm.Location = new System.Drawing.Point(74, 12);
+            this.TextboxDestAsm.Name = "TextboxDestAsm";
+            this.TextboxDestAsm.Size = new System.Drawing.Size(551, 19);
+            this.TextboxDestAsm.TabIndex = 0;
+            this.TextboxDestAsm.DragDrop += new System.Windows.Forms.DragEventHandler(this.TextboxDestAsm_DragDrop);
+            this.TextboxDestAsm.DragEnter += new System.Windows.Forms.DragEventHandler(this.TextboxDestAsm_DragEnter);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 12);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Dest Asm";
+            // 
+            // ButtonLoadDestAsm
+            // 
+            this.ButtonLoadDestAsm.Location = new System.Drawing.Point(631, 10);
+            this.ButtonLoadDestAsm.Name = "ButtonLoadDestAsm";
+            this.ButtonLoadDestAsm.Size = new System.Drawing.Size(75, 23);
+            this.ButtonLoadDestAsm.TabIndex = 2;
+            this.ButtonLoadDestAsm.Text = "選択";
+            this.ButtonLoadDestAsm.UseVisualStyleBackColor = true;
+            this.ButtonLoadDestAsm.Click += new System.EventHandler(this.ButtonLoadDestAsm_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(631, 94);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(718, 261);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.ButtonLoadDestAsm);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.TextboxDestAsm);
             this.Name = "Form1";
             this.Text = "SolidEdge Parts Manager";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox TextboxDestAsm;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button ButtonLoadDestAsm;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- src/WindowsFormsApplication1/Form1.cs
+++ src/WindowsFormsApplication1/Form1.cs
@@ -8,13 +8,195 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+using System.Reflection;
+using System.Runtime.InteropServices; // for com
+using IWshRuntimeLibrary; // for shortcuts, needs windows script host object model
+
+using System.Xml;
+using System.IO;
+//using Google.GData.Client;
+//using Google.GData.Spreadsheets;
+
+
+
 namespace WindowsFormsApplication1
 {
     public partial class Form1 : Form
     {
+        bool debug;
+
         public Form1()
         {
             InitializeComponent();
+#if DEBUG
+            debug = true;
+#else
+            debug = false;
+#endif
+        }
+
+
+        private string ResolveShorcut(string filename)
+        {
+            //WshShellClass shell = new WshShellClass(); // WshShellClassの型でエラーが出る
+            //IWshShortcut shortcut = (IWshShortcut)shell.CreateShortcut(filename);
+            //return shortcut.TargetPath;
+            return "hoge";
+        }
+
+        private bool IsSolidEdge()
+        {
+            SolidEdgeFramework.Application app = null;
+            bool ret = false;
+
+            try
+            {
+                //connect to solidedge instance
+                app = (SolidEdgeFramework.Application)Marshal.GetActiveObject("SolidEdge.Application");
+                ret = true;
+            }
+            catch (System.Exception ex)
+            {
+                MessageBox.Show("SolidEdgeが見つかりません");
+            }
+            finally
+            {
+                if (app != null)
+                {
+                    Marshal.ReleaseComObject(app);
+                    app = null;
+                }
+            }
+            return ret;
+        }
+
+        private void TextboxDestAsm_DragDrop(object sender, DragEventArgs e)
+        {
+            string[] s = (string[])e.Data.GetData(DataFormats.FileDrop, false);
+            if (s.Length > 0)
+            {
+                if (debug) MessageBox.Show(s[0]);
+                string ext = System.IO.Path.GetExtension(s[0]);
+                if (ext == ".asm")
+                {
+                    TextboxDestAsm.Text = s[0];
+                }
+                else if (ext == ".lnk")
+                {
+                    string filename = ResolveShorcut(s[0]);
+                    if (System.IO.Path.GetExtension(filename) == ".asm")
+                    {
+                        TextboxDestAsm.Text = s[0];
+                    }
+                }
+            }
+        }
+
+        private void TextboxDestAsm_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+                e.Effect = DragDropEffects.All;
+            else
+                e.Effect = DragDropEffects.None;
+        }
+
+        private void ButtonLoadDestAsm_Click(object sender, EventArgs e)
+        {
+            // OpenFileDialog の新しいインスタンスを生成する (デザイナから追加している場合は必要ない)
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Title = "Asmを追加";
+            dialog.Filter = "asmファル|*.asm";
+            dialog.Multiselect = false;
+
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                TextboxDestAsm.Text = dialog.FileName;
+            }
+            dialog.Dispose();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(TextboxDestAsm.Text);
+
+            if (!IsSolidEdge()) return;
+            getChildFile();
+        }
+
+        private void getChildFile()
+        {
+            SolidEdgeFramework.Application application = null;
+            SolidEdgeFramework.Documents documents = null;
+            SolidEdgeDraft.DraftDocument draft = null;
+            bool IsOverwrite = CheckboxIsOverwrite.Checked;
+            bool ret = false;
+
+            try
+            {
+                //check if the file exists
+                if (!System.IO.File.Exists(filename))
+                    throw (new System.Exception("file not found: " + filename));
+
+                //check if the directory
+                if (!System.IO.Directory.Exists(destdir))
+                    throw (new System.Exception("directory not found: " + destdir));
+
+                //check if the file ext is dft
+                if (System.IO.Path.GetExtension(filename) != ".dft")
+                    throw (new System.Exception("This is not a Draft file: " + filename));
+
+                //connect to solidedge instance
+                application = (SolidEdgeFramework.Application)Marshal.GetActiveObject("SolidEdge.Application");
+                documents = application.Documents;
+                if (debug) MessageBox.Show("solid edge found");
+
+                if (debug) MessageBox.Show("open draft");
+                //draft = (SolidEdgeDraft.DraftDocument)documents.Add("SolidEdge.DraftDocument", Missing.Value);
+                draft = (SolidEdgeDraft.DraftDocument)documents.Open(filename);
+
+
+                string outfile = destdir + "\\" + System.IO.Path.GetFileNameWithoutExtension(filename) + "." + ext;
+                if (debug) MessageBox.Show("filename: " + outfile);
+
+                if (IsOverwrite)
+                {
+                    //delete exist file
+                    if (debug) MessageBox.Show("delete file");
+                    System.IO.File.Delete(outfile);
+                }
+
+                if (debug) MessageBox.Show("save file");
+                draft.SaveAs(outfile);
+
+                if (debug) MessageBox.Show("close draft");
+                draft.Close(false);
+
+                ret = true;
+            }
+            catch (System.Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                if (draft != null)
+                {
+                    Marshal.ReleaseComObject(draft);
+                    draft = null;
+                }
+                if (documents != null)
+                {
+                    Marshal.ReleaseComObject(documents);
+                    documents = null;
+                }
+                if (application != null)
+                {
+                    Marshal.ReleaseComObject(application);
+                    application = null;
+                }
+            }
+
+            return;
         }
     }
 }
