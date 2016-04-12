8541258bd3c5ccc64907905b3917e7deded6a58a
kindsenior <kunio.einstein@gmail.com>
2016-04-12 21:35:32 +0900

    add CustomedSettings class

---
+++ src/WindowsFormsApplication1/CustomedSettings.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Configuration;
+
+namespace WindowsFormsApplication1
+{
+    public class CustomedSettings : ApplicationSettingsBase
+    {
+        public CustomedSettings()
+        {
+            DestSheetDataHashSet = new HashSet<SheetData>();
+        }
+
+        [UserScopedSetting()]
+        public HashSet<SheetData>DestSheetDataHashSet
+        {
+            get
+            {
+                return (HashSet<SheetData>)this["DestSheetDataHashSet"];
+            }
+            set
+            {
+                this["DestSheetDataHashSet"] = value;
+            }
+        }
+    }
+}
